89a000729b31a4140399e32ac6432f2ebbd328b6
Eva Innerlohninger <evainnerlohninger@einner.local>
2020-01-21 15:05:32 +0100

    add Script

---
+++ Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+
+    void Start() {
+        
+        if(x>0)
+        {
+            Debug.Log("x is bigger than 0");
+
+        }
+        else
+        {
+            Debug.Log("x is smaller than 0");
+
+        }
+
+    if (text == "UwU")
+    {
+        Debug.Log("UwU means 'I love You' in Dinosaur.");
+    }
+        
+    }
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# game-Worms-inner
+# Unity2019114f1-3D
+
+### Project description: 
+
+
+### Development platform: 
+MacOs High Sierra 10.13.6, Visual Studio Code, Unity 2019.1.14f1
+
+### Target platform: 
+WebGL
+
+### Visuals: 
+
+
+### Necessary setup/execution steps: 
+Unity installed, Visual Studio installed
+
+### Third party material: 
+
+
+### Project state: 
+
+
+### Limitations: 
+
+
+### Lessons Learned:
+
+
+
+Copyright by inner-e
