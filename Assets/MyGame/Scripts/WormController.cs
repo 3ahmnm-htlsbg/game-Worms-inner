8b91e707ce13b3e50d73d5392aaf72d8a58e6a7d
Eva Innerlohninger <evainnerlohninger@einner.local>
2020-02-06 08:26:00 +0100

    wrote Script

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -2,44 +2,49 @@
 
 public class WormController : MonoBehaviour
 {
-    public int x;
-    public string text;
+ 
 
-    void Start() {
-        
-        if(x>0)
-        {
-            Debug.Log("x is bigger than 0");
-
-        }
-        else
-        {
-            Debug.Log("x is smaller than 0");
-
-        }
-
-    if (text == "help")
+   void Update()
     {
-        Debug.Log("I will not help you, sorry.");
-    }
-
-    if (x<10)
-{
-  Debug.Log("x is smaller than 10, how bad."); 
-}
-else
-{
-    Debug.Log("x is 10 or over 10, how amazing!"); 
-}
         
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+
+           // Worm(transform.position);
+        }
+        
+        if (Input.GetKeyDown("space")){
+        
+            print("space key was pressed");
+        }
+  
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+
+            // Give the cloned object an initial velocity along the current
+            // object's Z axis
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+    void Start() {
+        rb = GetComponent<Rigidbody>();
     }
+    Start();
 
-
-
-
-
-
-
-
+    void FixedUpdate(){
+        rb.AddForce(transform.forward * thrust);
+    }
+    FixedUpdate();
+}
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+    public Rigidbody projectile;
 
 }
+
+
+
+
+
+
