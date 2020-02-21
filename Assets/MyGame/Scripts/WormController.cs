d3abb23478bb1f4c40a53f3cbb4c7c9a016c8df1
Eva Innerlohninger <evainnerlohninger@einner.local>
2020-02-21 09:51:35 +0100

    add BulletShooting

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class WormController : MonoBehaviour
 {
@@ -6,9 +8,9 @@ public class WormController : MonoBehaviour
     public float thrust = 20.0f;
     public float jump = 400.0f;
     public Rigidbody rb;
-    public Rigidbody projectile;
+    public Rigidbody Projectiles;
 
-   void Update()
+   private void Update()
     {
 
         
@@ -30,20 +32,32 @@ public class WormController : MonoBehaviour
              Vector3 direction = new Vector3(0,1,0);
             rb.AddForce(direction * jump);
         }
-  
 
-   
+      if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBall();
+        }
 
-    
-    
-       
-    
+    }
+
+    private void ShootBall()
+    {
+        
+        Vector3 BulletPos = rb.transform.position + new Vector3(1, 0, 0);
+
+        Rigidbody newbullet = Instantiate(Projectiles, BulletPos, Quaternion.identity);
+
+        Rigidbody ballRigid = newbullet.GetComponent<Rigidbody>();
+
+        ballRigid.AddForce(transform.right * jump);
+    }
+
    
 }
     
 
      
-}
+
 
 
 
