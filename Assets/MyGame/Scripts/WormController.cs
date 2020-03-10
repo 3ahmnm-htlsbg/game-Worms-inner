af5e7616d70cf85c0ae8801a1798086d22b0e638
Eva Innerlohninger <evainnerlohninger@einner.local>
2020-03-10 08:45:14 +0100

    second player is working

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -7,36 +7,46 @@ public class WormController : MonoBehaviour
  
     public float thrust = 20.0f;
     public float jump = 400.0f;
-    public float shoot = 50.0f;
-    public Rigidbody rb;
-    public Rigidbody Projectiles;
+    public float shoot = 100.0f;
+    public float shootRight = -100.0f;
 
-    public GameObject Gun;
-    public Transform ProjectilesPosition;
+    public Rigidbody rbLeft;
+    public Rigidbody rbRight;
 
-    public GameObject Sphere;
+    public Rigidbody ProjectilesLeft;
+    public Rigidbody ProjectilesRight;
+
+    public GameObject GunLeft;
+    public GameObject GunRight;
+
+    public Transform ProjectilesPositionL;
+    public Transform ProjectilesPositionR;
+
+    public GameObject SphereL;
+    public GameObject SphereR;
 
    private void Update()
     {
 
-        
+      //Left
+
         if (Input.GetKey(KeyCode.D)){
 
             Vector3 direction = new Vector3(1,0,0);
-            rb.AddForce(direction * thrust);
+            rbLeft.AddForce(direction * thrust);
         }
 
         
         if (Input.GetKey(KeyCode.A)){
         
              Vector3 direction = new Vector3(-1,0,0);
-            rb.AddForce(direction * thrust);
+            rbLeft.AddForce(direction * thrust);
         }
 
          if (Input.GetKeyDown(KeyCode.W)){
         
              Vector3 direction = new Vector3(0,1,0);
-            rb.AddForce(direction * jump);
+            rbLeft.AddForce(direction * jump);
         }
 
       if (Input.GetKeyDown(KeyCode.Space))
@@ -46,25 +56,63 @@ public class WormController : MonoBehaviour
          if (Input.GetKeyDown(KeyCode.Q))
             {
                 Debug.Log("Q is to rotate left");
-                Gun.transform.Rotate(0, 0, -10, Space.Self);
+                GunLeft.transform.Rotate(0, 0, -10, Space.Self);
             }
 
         
             if (Input.GetKeyDown(KeyCode.E))
             {
                 Debug.Log("E is to rotate right");
-                Gun.transform.Rotate(0, 0, 10, Space.Self);
+                GunLeft.transform.Rotate(0, 0, 10, Space.Self);
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-             GameObject Projectiles = Instantiate(Sphere, ProjectilesPosition.position, Quaternion.identity);
-             Projectiles.GetComponent<Rigidbody>().AddForce(Gun.transform.up * shoot);
+             GameObject ProjectilesLeft = Instantiate(SphereL, ProjectilesPositionL.position, Quaternion.identity);
+             ProjectilesLeft.GetComponent<Rigidbody>().AddForce(GunLeft.transform.up * shoot);
+            }
+
+            
+             //Right
+        
+        if (Input.GetKey(KeyCode.RightArrow)){
+
+            Vector3 direction = new Vector3(1,0,0);
+            rbRight.AddForce(direction * thrust);
+        }
+
+        
+        if (Input.GetKey(KeyCode.LeftArrow)){
+        
+             Vector3 direction = new Vector3(-1,0,0);
+            rbRight.AddForce(direction * thrust);
+        }
+
+         if (Input.GetKeyDown(KeyCode.UpArrow)){
+        
+             Vector3 direction = new Vector3(0,1,0);
+            rbRight.AddForce(direction * jump);
+        }
+
+      if (Input.GetKeyDown(KeyCode.L))
+        {
+            GameObject ProjectilesRight = Instantiate(SphereR, ProjectilesPositionR.position, Quaternion.identity);
+             ProjectilesRight.GetComponent<Rigidbody>().AddForce(GunRight.transform.up * shootRight);
+        }
+         if (Input.GetKeyDown(KeyCode.O))
+            {
+               
+                GunRight.transform.Rotate(0, 0, -10, Space.Self);
+            }
+
+        
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+          
+                GunRight.transform.Rotate(0, 0, 10, Space.Self);
             }
     }
  
-
-   
         
    
        // DisplayHealth();
